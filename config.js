7eda91b0f16dfd6dd0a8eb5edb50c033f64b388a
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2021-12-12 13:34:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['628221792667', '628221792667', '628221792667'] // Letakan nomor kamu disini
+global.owner = ['6282221792667', '6282221792667', '6282221792667'] // Letakan nomor kamu disini
 global.mods = [] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
