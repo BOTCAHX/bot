dccddbfb53a14feed372f4d4be5682290f1aa780
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2021-12-12 13:15:08 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ 
-├ 
+├ Selagi Gua ada kuota dan bisa run gratis cums buat have fun aja
+├ buat nanya run gua run di replit.
 └────
 `.trim(), '© stikerin', 'Donasi', '.donasi', m)
 handler.help = ['donasi']
